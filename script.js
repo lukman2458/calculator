eee9cc68507297e7a66377237f3e2b12ab7451c6
lukman2458 <@lukman2458.com>
2026-07-30 00:39:59 +0530

    make calculator working one calculation crctly, nxt need to fix further calculations

--- script.js
+++ script.js
@@ -1,81 +1,46 @@
 const buttons = document.querySelectorAll(".btn");
 const numbuttons = document.querySelectorAll(".numbtn")
 let flagfora = true;
-let temp = "";
+let temp1 = "";
+let temp2 = "";
 let answer;
-let a;
-let b;
-if(flagfora){
+let operand1;
+let operator;
+let operand2;
     buttons.forEach((button) => {
         button.addEventListener("click",function value(){
             if(button.classList.contains("operator")){
                 console.log(button.id)
-                a = +(temp);
+                operand1 = +(temp1);
                 flagfora = false;
-                console.log(flagfora)
-                if(button.id == "+"){
-                    return add();
+                operator = button.id;
+            }
+            else if(flagfora){
+                temp1 += button.id;
+                console.log(temp1);
+            }
+            else if(flagfora == false && button.id != "="){
+                temp2 += button.id;
+                console.log(temp2);
+            }
+            else if(button.id == "="){
+                operand2 = +(temp2);
+                if(operator == "+"){
+                    console.log(operand1 + operand2);
+                    temp1 = String(operand1 + operand2);
+                    flagfora = true;
+                    return temp1; 
                 }
-                else if(button.id == "-"){
-                    return sub();
+                else if(operator == "-"){
+                    return console.log(operand1 - operand2);
                 }
-                else if(button.id == "*"){
-                    return multiply();
+                else if(operator == "*"){
+                    return console.log(operand1 * operand2);
                 }
                 else{
-                    return divide();
+                    return console.log(operand1 / operand2);
                 }
             }
-            else{
-                temp += button.id;
-                console.log(temp);
-            }
         })
     })
-}
-else{
-    function add(a){
-        numbuttons.forEach((buttonx)=>{
-            buttonx.addEventListener("click",()=>{
-                console.log(buttonx.id)
-                b = +(buttonx.id);
-                console.log(a+b);
-                return;
-            })
-        })
-}
-function sub(a){
-    numbuttons.forEach((buttonx)=>{
-        buttonx.addEventListener("click",()=>{
-            console.log(flagfora)
-            console.log(buttonx.id)
-            b = +(buttonx.id);
-            console.log(a-b);
-            return;
-        })
-    })
-}
-function multiply(a){
-    numbuttons.forEach((buttonx)=>{
-        buttonx.addEventListener("click",()=>{
-            console.log(flagfora)
-            console.log(buttonx.id)
-            b = +(buttonx.id);
-            console.log(a*b);
-            return;
-        })
-    })
-}
-function divide(a){
-    numbuttons.forEach((buttonx)=>{
-        buttonx.addEventListener("click",()=>{
-            console.log(flagfora)
-            console.log(buttonx.id)
-            b = +(buttonx.id);
-            console.log(a/b);
-            return;
-        })
-    })
-}
-}
 
